8b8341ec22efae6fc9df0b130f6a088777606aa6
michlSz <54441422+michlSz@users.noreply.github.com>
2021-01-28 12:05:17 +0100

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
 <?php
 
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
+define("DB_HOST", "");
+define("DB_USER", "");
 define("DB_PASS", "");
-define("DB_NAME", "signal2");
+define("DB_NAME", "");
 
 
 
